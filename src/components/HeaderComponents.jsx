3fc6c1ab7b79b2371b4b2e69722dc059d0d2292f
munawaror <medinatabdulkareem25@gmail.com>
2025-07-07 06:52:56 -0500

    feat: added active state to navbar

--- src/components/HeaderComponents.jsx
+++ src/components/HeaderComponents.jsx
@@ -3,9 +3,11 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import React, { useState } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const HeaderComponent = () => {
   const [toggleMobileNav, setToggleMobileNav] = useState(false);
+  const currentPage = usePathname();
 
   const handleToggleMobileNav = () => {
     setToggleMobileNav(!toggleMobileNav);
@@ -13,9 +15,11 @@ const HeaderComponent = () => {
 
   return (
     <header className="w-full flex items-center justify-between h-[86px]">
-        <div className="w-fit flex items-center-safe justify-start">
-        <Link href='/' ><h1 className="font-bold text-xl text-black">Meddi</h1></Link>
-        </div>
+      <div className="w-fit flex items-center-safe justify-start">
+        <Link href="/">
+          <h1 className="font-bold text-xl text-black">Meddi</h1>
+        </Link>
+      </div>
       <div className="w-full flex sm:hidden items-center justify-end px-2.5 relative">
         <button type="button" onClick={handleToggleMobileNav}>
           <RxHamburgerMenu size="30px" height="18px" />
@@ -47,19 +51,25 @@ const HeaderComponent = () => {
       </div>
       <nav className="w-[236px] hidden sm:flex items-center justify-between ">
         <Link
-          className="text-[#000000] text-xl font-medium cursor-pointer"
-          href="/blog"
-        >
-          Blog
-        </Link>
-        <Link
-          className="text-[#000000] text-xl font-medium cursor-pointer"
+          className={` ${
+            currentPage.includes("work") ? "text-[#FF6464]" : "text-[#000000]"
+          } text-xl font-medium cursor-pointer`}
           href="/work"
         >
           Works
         </Link>
         <Link
-          className="text-[#000000] text-xl font-medium cursor-pointer"
+          className={` ${
+            currentPage.includes("blog") ? "text-[#FF6464]" : "text-[#000000]"
+          } text-xl font-medium cursor-pointer`}
+          href="/blog"
+        >
+          Blog
+        </Link>
+        <Link
+          className={` ${
+            currentPage.includes("contact") ? "text-[#FF6464]" : "text-[#000000]"
+          } text-xl font-medium cursor-pointer`}
           href="/contact"
         >
           Contact
