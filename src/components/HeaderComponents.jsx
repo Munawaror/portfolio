6187ecb370a093f36c183630a315daebee7c900e
munawaror <medinatabdulkareem25@gmail.com>
2025-07-07 06:38:55 -0500

    feat: added custom logo

--- src/components/HeaderComponents.jsx
+++ src/components/HeaderComponents.jsx
@@ -1,46 +1,72 @@
-'use client'
+"use client";
 
 import { RxHamburgerMenu } from "react-icons/rx";
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import Link from "next/link";
 
 const HeaderComponent = () => {
-    const [toggleMobileNav, setToggleMobileNav] = useState(false);
+  const [toggleMobileNav, setToggleMobileNav] = useState(false);
 
-    const handleToggleMobileNav = () => {
-        setToggleMobileNav(!toggleMobileNav)
-    }
+  const handleToggleMobileNav = () => {
+    setToggleMobileNav(!toggleMobileNav);
+  };
 
-    return (
-        <header className='w-full flex items-center justify-end h-[86px]'>
-            <div className='w-full flex sm:hidden items-center justify-end px-2.5 relative'>
-                <button type='button' onClick={handleToggleMobileNav}>
-                    <RxHamburgerMenu size='30px' height='18px' />
-                </button>
-                {
-                    toggleMobileNav && (
-                        <div className='w-[360px] p-6 mx-auto absolute top-7 right-6 z-10 bg-white shadow-lg'>
-                            <nav className='w-full flex flex-col items-center justify-between gap-6'>
-                                <Link className='text-[#000000] text-xl font-medium cursor-pointer' href='work'>Works</Link>
-                                <Link className='text-[#000000] text-xl font-medium cursor-pointer' href='blog'>Blog</Link>
-                                <Link className='text-[#000000] text-xl font-medium cursor-pointer' href='contact'>Contact</Link>
-                            </nav>
-                        </div>
-                    )
-                }
-
-            </div>
-            <nav className='w-[236px] hidden sm:flex items-center justify-between '>
-                <Link className='text-[#000000] text-xl font-medium cursor-pointer' href='blog'>Blog</Link>
-                <Link className='text-[#000000] text-xl font-medium cursor-pointer' href='work'>Works</Link>
-                <Link className='text-[#000000] text-xl font-medium cursor-pointer' href='contact'>Contact</Link>
+  return (
+    <header className="w-full flex items-center justify-between h-[86px]">
+        <div className="w-fit flex items-center-safe justify-start">
+        <Link href='/' ><h1 className="font-bold text-xl text-black">Meddi</h1></Link>
+        </div>
+      <div className="w-full flex sm:hidden items-center justify-end px-2.5 relative">
+        <button type="button" onClick={handleToggleMobileNav}>
+          <RxHamburgerMenu size="30px" height="18px" />
+        </button>
+        {toggleMobileNav && (
+          <div className="w-[360px] p-6 mx-auto absolute top-7 right-6 z-10 bg-white shadow-lg">
+            <nav className="w-full flex flex-col items-center justify-between gap-6">
+              <Link
+                className="text-[#000000] text-xl font-medium cursor-pointer"
+                href="/work"
+              >
+                Works
+              </Link>
+              <Link
+                className="text-[#000000] text-xl font-medium cursor-pointer"
+                href="/blog"
+              >
+                Blog
+              </Link>
+              <Link
+                className="text-[#000000] text-xl font-medium cursor-pointer"
+                href="/contact"
+              >
+                Contact
+              </Link>
             </nav>
+          </div>
+        )}
+      </div>
+      <nav className="w-[236px] hidden sm:flex items-center justify-between ">
+        <Link
+          className="text-[#000000] text-xl font-medium cursor-pointer"
+          href="/blog"
+        >
+          Blog
+        </Link>
+        <Link
+          className="text-[#000000] text-xl font-medium cursor-pointer"
+          href="/work"
+        >
+          Works
+        </Link>
+        <Link
+          className="text-[#000000] text-xl font-medium cursor-pointer"
+          href="/contact"
+        >
+          Contact
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
-            
-
-
-        </header>
-    )
-}
-
-export default HeaderComponent
+export default HeaderComponent;
